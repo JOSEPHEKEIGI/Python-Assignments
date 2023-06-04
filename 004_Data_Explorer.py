1ff22ebff9e0ec129a8a252eb906a12d74cad8e9
Joseph Keigi <Joseph.keigi2015@gmail.com>
2023-06-04 22:59:26 +0300

    Update assignment

--- 004_Data_Explorer.py
+++ 004_Data_Explorer.py
@@ -1,2 +1,21 @@
 import streamlit as st
+import io
+import pandas as pd
+import plotly.express as px
+import os
+
+#set page
+st.set_page_config(
+    layout='wide',
+    initial_sidebar_state='expanded',
+    page_icon='WERT',
+    page_title='Data Explorer')
+
+#Load data
+path = os.path.dirname(__file__)
+path = os.path.join(path,"iris.data")
+
+st.header('Data Exploreer')
+
+sidebr = st.sidebar.file_uploader('Upload dataset',['csv','.data','*lsx','txt'])
 
