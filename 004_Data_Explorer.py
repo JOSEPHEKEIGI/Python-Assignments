27df815d6297558a594a684046d356f3dc0e59b2
Joseph Keigi <Joseph.keigi2015@gmail.com>
2023-06-05 21:47:43 +0300

    Updating Answer

--- 004_Data_Explorer.py
+++ 004_Data_Explorer.py
@@ -11,11 +11,34 @@ st.set_page_config(
     page_icon='WERT',
     page_title='Data Explorer')
 
+st.sidebar.header('Data Explorer')
+
 #Load data
-path = os.path.dirname(__file__)
-path = os.path.join(path,"iris.data")
+# path = os.path.dirname(__file__)
+# path = os.path.join(path,"iris.data")
 
-st.header('Data Exploreer')
 
-sidebr = st.sidebar.file_uploader('Upload dataset',['csv','.data','*lsx','txt'])
 
+data = st.sidebar.file_uploader('Upload dataset',['csv','.data','*lsx','txt'])
+
+
+
+if data is not None:
+    df = pd.read_csv(data, sep=',',header=None)
+    df.columns = ['Sepal_length', 'Sepal_width', 'Petal_length', 'Petal_width','Species']     
+    st.header('Sample of Data')
+    st.write(df.head(5))    
+    st.write()
+
+    cols1, cols2 = st.columns(2)
+
+    with cols1:
+        st.subheader("Average Sepal of each species")
+        Avg_sepal_length = df.groupby( "Species" )['Sepal_length'].sum()
+        st.write(Avg_sepal_length)
+
+    with cols2:
+        fig = px.scatter(x= df['Sepal_length'], y = df['Sepal_width'])
+
+        fig.show()
+  
